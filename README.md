47dd02c6ebfd1d95909a1f8bc26a9367fb82015e
Thomas CASTELLY <thomas@tcy.io>
2022-03-23 10:48:32 +0100

    add arm architecture

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # docker-node
 Extends official Node with oci8 extension
+
+# platforms
+- x86_64
+- arm64
---
+++ oracle_client/install_oracle_driver.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## in function of architecture, download and install expected archive
+cd /opt/oracle
+
+if [ `arch` == "x86_64" ];
+  echo "download and install for x86_64"
+  unzip ./instantclient-basiclite-linux.x64-19.3.0.0.0dbru.zip -d ./unzip && \
+    unzip ./instantclient-sdk-linux.x64-19.3.0.0.0dbru.zip -d ./unzip && \
+    rm ./instantclient-basiclite-linux.x64-19.3.0.0.0dbru.zip instantclient-sdk-linux.x64-19.3.0.0.0dbru.zip &&\
+    mv ./unzip/instantclient_19_3 ./unzip/instantclient
+then
+  echo "download and install for arm"
+  unzip ./instantclient-basiclite-linux.arm64-19.10.0.0.0dbru.zip -d ./unzip && \
+    unzip ./instantclient-sdk-linux.arm64-19.10.0.0.0dbru.zip -d ./unzip && \
+    rm ./instantclient-basiclite-linux.arm64-19.10.0.0.0dbru.zip ./instantclient-sdk-linux.arm64-19.10.0.0.0dbru.zip &&\
+    mv ./unzip/instantclient_19_10 ./unzip/instantclient
+fi
+
